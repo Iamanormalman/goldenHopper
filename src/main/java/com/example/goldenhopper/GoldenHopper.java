027366f8344d0d94d873c5c373937c6898d68f5c
Iamanormalman <linbanana9890@gmail.com>
2025-08-16 22:37:06 +0800

    Initial commit

---
+++ src/main/java/com/example/goldenhopper/GoldenHopper.java
@@ -0,0 +1,33 @@
+package com.example.goldenhopper;
+
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = GoldenHopper.MODID, version = GoldenHopper.VERSION)
+public class GoldenHopper
+{
+    public static final String MODID = "goldenhopper";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
