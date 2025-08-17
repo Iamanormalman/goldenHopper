b1337dc73717123f224eab727aed6fc09d8f8853
Iamanormalman <linbanana9890@gmail.com>
2025-08-17 13:21:33 +0800

    New commit

--- src/main/java/com/example/goldenhopper/GoldenHopper.java
+++ src/main/java/com/example/goldenhopper/GoldenHopper.java
@@ -7,11 +7,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
 @Mod(modid = GoldenHopper.MODID, version = GoldenHopper.VERSION)
